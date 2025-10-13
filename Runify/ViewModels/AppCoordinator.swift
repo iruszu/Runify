ba3778ad0ce6ebd4043b86019eb2b7327a48ce41
Kellie <kellieh.exe@gmail.com>
2025-10-12 17:04:45 -0700

    refactor: Simplify AppCoordinator navigation state
    - Remove AppRoute enum dependency
    - Use integer-based tab selection for better compatibility
    - Streamline navigation methods and state management
    - Improve TabView integration

--- Runify/ViewModels/AppCoordinator.swift
+++ Runify/ViewModels/AppCoordinator.swift
@@ -1,17 +1,8 @@
 import SwiftUI
 import Foundation
 
-enum AppRoute {
-    case home
-    case countdown
-    case run
-    case runSummary
-    case runningMap
-}
-
 class AppCoordinator: ObservableObject {
-    @Published var currentRoute: AppRoute = .home
-    @Published var navigationStack: [AppRoute] = []
+    @Published var navigationStack: [Int] = []
     
     // Navigation state
     @Published var showCountdown = false
@@ -21,45 +12,26 @@ class AppCoordinator: ObservableObject {
     
     // Navigation methods
     func navigateToCountdown() {
-        currentRoute = .countdown
         showCountdown = true
     }
     
     func countdownFinished() {
         showCountdown = false
-        currentRoute = .runningMap
         showRunningMap = true
     }
-    
-    func startRun() {
-        currentRoute = .run
-        showRun = true
-    }
+
     
     func stopRun() {
         showRunningMap = false
-        currentRoute = .runSummary
         showRunSummary = true
     }
     
     func finishRunSummary() {
         showRunSummary = false
-        currentRoute = .home
     }
-    
-    func goBack() {
-        if !navigationStack.isEmpty {
-            navigationStack.removeLast()
-            if let previousRoute = navigationStack.last {
-                currentRoute = previousRoute
-            } else {
-                currentRoute = .home
-            }
-        }
-    }
+
     
     func resetToHome() {
-        currentRoute = .home
         navigationStack.removeAll()
         showCountdown = false
         showRun = false
